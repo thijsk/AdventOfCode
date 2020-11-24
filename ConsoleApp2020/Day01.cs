084ab79349b335a09276494df66f514f1aedc2ce
Thijs Kroesbergen <ta.kroesbergen@belastingdienst.nl>
2020-11-24 16:36:50 +0100

    Upgrade to .NET 5 / ready for 2020

---
+++ ConsoleApp2020/Day01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace ConsoleApp2020
+{
+    class Day01 : IDay
+    {
+        public long Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp2020/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Common;
+
+namespace ConsoleApp2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dayClasses = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(mytype => mytype.GetInterfaces().Contains(typeof(IDay))).OrderBy(t => int.Parse(t.Name.Substring(3)));
+
+            foreach (var dayClass in dayClasses.TakeLast(1))
+            {
+                Console.WriteLine(dayClass.Name);
+
+                IDay day = (IDay)Activator.CreateInstance(dayClass);
+                Console.WriteLine("Part1 : " + day.Part1());
+                Console.WriteLine("Part2 : " + day.Part2());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
