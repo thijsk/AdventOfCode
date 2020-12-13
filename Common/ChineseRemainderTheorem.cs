ea6176a4a924933d7d12b0f2988501f648907d4a
Thijs Kroesbergen <thijs@brokenwire.net>
2020-12-13 09:34:34 +0100

    Day 13 ; impossible

---
+++ Common/ChineseRemainderTheorem.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Numerics;
+
+namespace Common
+{
+    public static class ChineseRemainderTheorem
+    {
+        public static long Solve(int[] n, int[] a)
+        {
+            long prod = n.Aggregate(1, (i, j) => i * j);
+            long p;
+            long sm = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                p = prod / n[i];
+                sm += a[i] * ModularMultiplicativeInverse(p, n[i]) * p;
+            }
+            return sm % prod;
+        }
+
+        private static long ModularMultiplicativeInverse(long a, int mod)
+        {
+            long b = a % mod;
+            for (int x = 1; x < mod; x++)
+            {
+                if ((b * x) % mod == 1)
+                {
+                    return x;
+                }
+            }
+            return 1;
+        }
+    }
+
+}
---
+++ ConsoleApp2020/Day13.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace ConsoleApp2020
+{
+    class Day13 : IDay
+    {
+        public long Part1()
+        {
+            var input = ParseInput();
+
+            var time = input.Item1;
+
+            while (true)
+            {
+                foreach (var bus in input.Item2)
+                {
+                    if (bus != -1)
+                    {
+                        if (time % bus == 0)
+                        {
+                            return bus * (time - input.Item1);
+                        }
+                    }
+                }
+
+                time++;
+            }
+ 
+            return 0;
+        }
+
+        public long Part2()
+        {
+            var input = ParseInput();
+            var bustable = input.Item2.ToArray();
+            var busses = new List<int>();
+            var offsets = new List<int>();
+            var a = new List<int>();
+            for (int i = 0; i < bustable.Length; i++)
+            {
+                var bus = bustable[i];
+                if (bus != -1)
+                {
+                    busses.Add(bus);
+                    offsets.Add(i);
+                }
+            }
+
+            Console.WriteLine("Put this in wolfram alpha:");
+            for (int i = 0; i < busses.Count; i++)
+            {
+
+                if (i > 0)
+                    Console.Write(", ");
+                Console.Write($"(t + {offsets[i]}) mod {busses[i]} = 0");
+
+            }
+
+            Console.WriteLine();
+
+            long time = 0;
+            long advanceBy = busses[0];
+            int correctBusIndex = 0;
+            while (true)
+            {
+                bool found = true;
+                for (int i = correctBusIndex + 1; i < busses.Count; i++)
+                {
+                    int bus = busses[i];
+                    int offset = offsets[i] % busses[i];
+                    if (TimeLeft(bus, time) == offset)
+                    {
+                        advanceBy *= bus;
+                        correctBusIndex = i;
+                    }
+                    else
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    return time;
+                }
+
+                time += advanceBy;
+            }
+        }
+
+
+        static int TimeLeft(int bus, long time)
+        {
+            int timeLeft = (int)(time % bus);
+            if (timeLeft > 0)
+            {
+                timeLeft = bus - timeLeft;
+            }
+            return timeLeft;
+        }
+
+        public (int, IEnumerable<int>) ParseInput()
+        {
+            var lines = File.ReadAllLines($"Day13.txt");
+            var time = int.Parse(lines[0]);
+            var busses = lines[1].Split(',').Select(b => b == "x" ? -1 : int.Parse(b));
+            return (time, busses);
+        }
+    }
+}
