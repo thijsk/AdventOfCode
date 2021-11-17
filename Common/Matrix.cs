e5dc9f0456d5b9f43aeb1fe560ed0fc3337bbc10
Thijs Kroesbergen <thijs@brokenwire.net>
2021-11-17 17:13:16 +0100

    25

---
+++ Common/Matrix.cs
@@ -0,0 +1,170 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Common
+{
+    public class Matrix<T>
+    {
+        T[,] _matrix;
+
+        Rotation _rotation;
+        private Flip _flip;
+
+        private int _xUpperbound;
+        private int _yUpperbound;
+
+        private enum Rotation
+        {
+            None,
+            Clockwise90,
+            Clockwise180,
+            Clockwise270
+        }
+
+        private enum Flip
+        {
+            None,
+            Flipped
+        }
+
+
+        public Matrix(T[,] matrix)
+        {
+            _matrix = matrix;
+            _rotation = Rotation.None;
+            _xUpperbound = _matrix.GetUpperBound(0);
+            _yUpperbound = _matrix.GetUpperBound(1);
+        }
+
+        //  the transformation routines
+        public void RotateClockwise90()
+        {
+            _rotation = (Rotation)(((int)_rotation + 1) & 3);
+        }
+
+        public void Rotate180()
+        {
+            _rotation = (Rotation)(((int)_rotation + 2) & 3);
+        }
+
+        public void RotateAntiClockwise90()
+        {
+            _rotation = (Rotation)(((int)_rotation + 3) & 3);
+        }
+
+        public void FlipMe()
+        {
+            _flip = (_flip == Flip.None ? Flip.Flipped : Flip.None);
+        }
+
+        //  accessor property to make class look like a two dimensional array
+        public T this[int row, int column]
+        {
+            get
+            {
+                var (x, y) = TranslatePosition(row, column);
+                return _matrix[x, y];
+            }
+
+            set
+            {
+                var (x, y) = TranslatePosition(row, column);
+                _matrix[x, y] = value;
+            }
+        }
+
+        private (int row, int column) TranslatePosition(int row, int column)
+        {
+            int outrow = 0;
+            int outcolumn = 0;
+
+            switch (_rotation)
+            {
+                case Rotation.None:
+                    outrow = row;
+                    outcolumn = column;
+                    break;
+
+                case Rotation.Clockwise90:
+                    outrow = _xUpperbound - column;
+                    outcolumn = row;
+                    break;
+
+                case Rotation.Clockwise180:
+                    outrow = _xUpperbound - row;
+                    outcolumn = _yUpperbound - column;
+                    break;
+
+                case Rotation.Clockwise270:
+                    outrow = column;
+                    outcolumn = _yUpperbound - row;
+                    break;
+            }
+
+            if (_flip == Flip.Flipped)
+            {
+                int tmp = outrow;
+                outrow = outcolumn;
+                outcolumn = tmp;
+            }
+
+            return (outrow, outcolumn);
+        }
+
+        //  creates a string with the matrix values
+        public override string ToString()
+        {
+            int
+                nu_rows = 0,
+                nu_columns = 0;
+
+            switch (_rotation)
+            {
+                case Rotation.None:
+                case Rotation.Clockwise180:
+                    nu_rows = _matrix.GetUpperBound(0);
+                    nu_columns = _matrix.GetUpperBound(1);
+                    break;
+
+                case Rotation.Clockwise90:
+                case Rotation.Clockwise270:
+                    nu_rows = _matrix.GetUpperBound(1);
+                    nu_columns = _matrix.GetUpperBound(0);
+                    break;
+            }
+
+            StringBuilder
+                output = new StringBuilder();
+
+            output.Append("{");
+
+            for (int row = 0; row <= nu_rows; ++row)
+            {
+                if (row != 0)
+                {
+                    output.Append(", ");
+                }
+
+                output.Append("{");
+
+                for (int column = 0; column <= nu_columns; ++column)
+                {
+                    if (column != 0)
+                    {
+                        output.Append(", ");
+                    }
+
+                    output.Append(this[row, column]);
+                }
+
+                output.Append("}");
+            }
+
+            output.Append("}");
+
+            return output.ToString();
+        }
+
+
+    }
+}
---
+++ ConsoleApp2020/Day25.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Common;
+
+namespace ConsoleApp2020
+{
+    class Day25 : IDay
+    {
+        public long Part1()
+        {
+            var input = ParseInput();
+
+            var cardSubjectNo = input[0];
+            var doorSubjectNo = input[1];
+
+            var cardLoops = SearchLoop(cardSubjectNo);
+            var doorLoops = SearchLoop(doorSubjectNo);
+
+
+            ConsoleX.WriteLine($"{cardLoops}, {doorLoops}");
+
+            var cardKey = Loop(doorSubjectNo, cardLoops);
+            var doorKey = Loop(cardSubjectNo, doorLoops);
+
+            ConsoleX.WriteLine($"{cardKey}, {doorKey}");
+
+            return cardKey;
+        }
+
+        //Set the value to itself multiplied by the subject number.
+        //Set the value to the remainder after dividing the value by 20201227.
+        private long Loop(long subjectNr, long loops)
+        {
+            long value = 1;
+            for (long loop = 1; loop <= loops; loop++)
+            {
+                value = value * subjectNr;
+                value = value % 20201227;
+            }
+
+            return value;
+        }
+
+        private long SearchLoop(long resultSubjectNr)
+        {
+            long subjectNr = 7;
+            long value = 1;
+            for (long loop = 1; loop <= long.MaxValue; loop++)
+            {
+                value = value * subjectNr;
+                value = value % 20201227;
+                if (value == resultSubjectNr)
+                    return loop;
+            }
+
+            return value;
+        }
+
+        public long Part2()
+        {
+            var input = ParseInput();
+
+            return 0;
+        }
+
+        public long[] ParseInput()
+        {
+            return File.ReadAllLines($"Day25.txt").Select(line => Convert.ToInt64(line)).ToArray();
+
+        }
+    }
+}
