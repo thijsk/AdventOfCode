2ed096e79286f5288062d0acb4ef94a3bf2edb5c
Thijs Kroesbergen <thijs@trilobit.nl>
2022-11-27 12:06:43 +0100

    Ready for 2022

---
+++ ConsoleApp2022/Day01.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+
+namespace ConsoleApp2022;
+
+public class Day01 : IDay
+{
+    public long Part1()
+    {
+        var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+        return 0;
+    }
+
+    public long Part2()
+    {
+        var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+        return 0;
+    }
+
+    public long Parse(string line)
+    {
+        return long.Parse(line);
+    }
+
+}
---
+++ ConsoleApp2022/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Common;
+using TextCopy;
+
+var dayClasses = Assembly.GetExecutingAssembly().GetTypes()
+    .Where(mytype => mytype.GetInterfaces().Contains(typeof(IDay))).OrderBy(t => int.Parse(t.Name[3..]));
+
+foreach (var dayClass in dayClasses
+             //.Where(d => d.Name == "Day23")
+             .TakeLast(1))
+{
+    Console.WriteLine(dayClass.Name);
+    var day = (IDay)Activator.CreateInstance(dayClass)!;
+
+    PuzzleContext.Input = File.ReadAllLines(dayClass.Name + ".txt");
+    if (File.Exists(dayClass.Name + "-example.txt"))
+    {
+        PuzzleContext.Example = File.ReadAllLines(dayClass.Name + "-example.txt");
+    }
+
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    var answer1 = day.Part1().ToString();
+    stopwatch.Stop();
+    ConsoleX.WriteLine("Part1   : " + answer1, ConsoleColor.Red);
+    Console.WriteLine($"Elapsed : {stopwatch.Elapsed}");
+    if (answer1 != "0") ClipboardService.SetText(answer1);
+
+    stopwatch.Restart();
+    var answer2 = day.Part2().ToString();
+    stopwatch.Stop();
+    ConsoleX.WriteLine("Part2   : " + answer2, ConsoleColor.Red);
+    Console.WriteLine($"Elapsed : {stopwatch.Elapsed}");
+    if (answer2 != "0") ClipboardService.SetText(answer2);
+}
