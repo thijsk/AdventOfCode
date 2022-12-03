2c3bfe2924ef9f4eeab4ef53686e50b775a8d648
Thijs Kroesbergen <thijs@trilobit.nl>
2022-12-03 06:29:14 +0100

    Day03

--- ConsoleApp2022/Day03.cs
+++ ConsoleApp2022/Day03.cs
@@ -8,24 +8,65 @@ public class Day03 : IDay
     {
         var input = PuzzleContext.Input.Select(Parse).ToArray();
 
-        foreach (var i in 1..10)
+        var sum = 0L;
+        foreach (var (first, second) in input)
         {
+            var overlap = first.Intersect(second).Single();
+            Console.WriteLine((int) overlap);
+            if (overlap is >= 'a' and <= 'z')
+            {
+                sum += (int) overlap - 96;
 
+            }
+
+            if (overlap is >= 'A' and <= 'Z')
+            {
+                sum += (int) overlap - 38;
+            }
         }
 
-        return 0;
+
+
+
+        return sum;
     }
 
     public long Part2()
     {
         var input = PuzzleContext.Input.Select(Parse).ToArray();
 
-        return 0;
+        var groups = input.Chunk(3);
+        var sum = 0L;
+        foreach (var group in groups)
+        {
+            var one = group[0].first.Concat(group[0].second).ToArray();
+            var two = group[1].first.Concat(group[1].second).ToArray();
+            var three = group[2].first.Concat(group[2].second).ToArray();
+
+            var badge = one.Intersect(two).Intersect(three).Single();
+            Console.WriteLine(badge);
+
+            if (badge is >= 'a' and <= 'z')
+            {
+                sum += (int)badge - 96;
+
+            }
+
+            if (badge is >= 'A' and <= 'Z')
+            {
+                sum += (int)badge - 38;
+            }
+        }
+        
+        return sum;
     }
 
-    public long Parse(string line)
+    public (char[] first, char[] second) Parse(string line)
     {
-        return long.Parse(line);
+        var items = line.ToCharArray();
+        var count = items.Length / 2;
+        var first = items.Take(count).ToArray();
+        var second = items.Skip(count).Take(count).ToArray();
+        return (first, second);
     }
-
 }
