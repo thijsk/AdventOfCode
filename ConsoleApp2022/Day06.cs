bbcf2095234d9d9beb9233ce5d4832b9c8ed66b5
Thijs Kroesbergen <thijs@trilobit.nl>
2022-12-06 09:15:53 +0100

    range in array indexer

--- ConsoleApp2022/Day06.cs
+++ ConsoleApp2022/Day06.cs
@@ -8,11 +8,11 @@ public class Day06 : IDay
     {
         var input = PuzzleContext.Input.First();
 
-        foreach (var index in 3..input.Length)
+        foreach (var index in 4..input.Length)
         {
-            if (input.Skip(index - 3).Take(4).GroupBy(c => c).Count() == 4)
+            if (input[(index - 4)..index].Distinct().Count() == 4)
             {
-                return index+1;
+                return index;
             }
         }
 
@@ -25,16 +25,13 @@ public class Day06 : IDay
         
         const int messageLength = 14;
 
-        foreach (var index in (messageLength-1)..input.Length)
+        foreach (var index in messageLength..input.Length)
         {
-            if (input.Skip(index - (messageLength - 1)).Take(messageLength).GroupBy(c => c).Count() == messageLength)
+            if (input[(index - messageLength)..index].Distinct().Count() == messageLength)
             {
-                return index + 1;
+                return index;
             }
         }
         return 0;
     }
-
-
-
 }
