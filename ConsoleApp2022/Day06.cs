c33aa380e06355422ef6e430f54e79d3d415f51f
Thijs Kroesbergen <thijs@trilobit.nl>
2022-12-06 06:21:51 +0100

    Cleanup

--- ConsoleApp2022/Day06.cs
+++ ConsoleApp2022/Day06.cs
@@ -6,18 +6,12 @@ public class Day06 : IDay
 {
     public long Part1()
     {
-        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"; //PuzzleContext.Input.First();
-        input = PuzzleContext.Input.First();
+        var input = PuzzleContext.Input.First();
 
         foreach (var index in 3..input.Length)
         {
-            var buffer = input.Substring(index - 3, 4);
-
-            var chars = buffer.ToCharArray().GroupBy(c => c).Count();
-
-            if (chars == 4)
+            if (input.Skip(index - 3).Take(4).GroupBy(c => c).Count() == 4)
             {
-                Console.WriteLine(buffer);
                 return index+1;
             }
         }
@@ -27,21 +21,14 @@ public class Day06 : IDay
 
     public long Part2()
     {
-        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"; //PuzzleContext.Input.First();
-        input = PuzzleContext.Input.First();
-
+        var input = PuzzleContext.Input.First();
+        
         const int messageLength = 14;
 
         foreach (var index in (messageLength-1)..input.Length)
         {
-            var buffer = input.Substring(index - (messageLength - 1), messageLength);
-            Console.WriteLine(index+1);
-            Console.WriteLine(buffer);
-            var chars = buffer.ToCharArray().GroupBy(c => c).Count();
-
-            if (chars == messageLength)
+            if (input.Skip(index - (messageLength - 1)).Take(messageLength).GroupBy(c => c).Count() == messageLength)
             {
-                Console.WriteLine(buffer);
                 return index + 1;
             }
         }
