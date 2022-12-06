a74fc9daba943a1294364ff84e05f16f2efa3e2a
Thijs Kroesbergen <thijs@trilobit.nl>
2022-12-06 06:11:49 +0100

    Day06

---
+++ ConsoleApp2022/Day06.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+
+namespace ConsoleApp2022;
+
+public class Day06 : IDay
+{
+    public long Part1()
+    {
+        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"; //PuzzleContext.Input.First();
+        input = PuzzleContext.Input.First();
+
+        foreach (var index in 3..input.Length)
+        {
+            var buffer = input.Substring(index - 3, 4);
+
+            var chars = buffer.ToCharArray().GroupBy(c => c).Count();
+
+            if (chars == 4)
+            {
+                Console.WriteLine(buffer);
+                return index+1;
+            }
+        }
+
+        return 0;
+    }
+
+    public long Part2()
+    {
+        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"; //PuzzleContext.Input.First();
+        input = PuzzleContext.Input.First();
+
+        const int messageLength = 14;
+
+        foreach (var index in (messageLength-1)..input.Length)
+        {
+            var buffer = input.Substring(index - (messageLength - 1), messageLength);
+            Console.WriteLine(index+1);
+            Console.WriteLine(buffer);
+            var chars = buffer.ToCharArray().GroupBy(c => c).Count();
+
+            if (chars == messageLength)
+            {
+                Console.WriteLine(buffer);
+                return index + 1;
+            }
+        }
+        return 0;
+    }
+
+
+
+}
