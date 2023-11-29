f98bdb57518dd36f6367ad597a1756fda279df2f
Thijs Kroesbergen <thijs@trilobit.nl>
2023-11-29 16:21:13 +0100

    Update to .net 8

---
+++ ConsoleApp2023/Day01.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+
+namespace ConsoleApp2023;
+
+public class Day01 : IDay
+{
+	public long Part1()
+	{
+		PuzzleContext.Answer1 = 0;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+		return 0;
+	}
+
+	public long Part2()
+	{
+		PuzzleContext.Answer2 = 0;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+		return 0;
+	}
+
+	public long Parse(string line)
+	{
+		return long.Parse(line);
+	}
+
+}
