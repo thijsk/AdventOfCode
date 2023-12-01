c2e13d312e9bfad7c95af0edf4f15dd2a82a9e34
Thijs Kroesbergen <thijs@trilobit.nl>
2023-12-01 10:58:37 +0100

    2023 day 1

--- ConsoleApp2023/Day01.cs
+++ ConsoleApp2023/Day01.cs
@@ -6,27 +6,55 @@ public class Day01 : IDay
 {
 	public long Part1()
 	{
-		PuzzleContext.Answer1 = 0;
+		PuzzleContext.Answer1 = 55607;
 		PuzzleContext.UseExample = false;
 
 		var input = PuzzleContext.Input.Select(Parse).ToArray();
 
-		return 0;
+		return input.Sum();
 	}
 
 	public long Part2()
 	{
-		PuzzleContext.Answer2 = 0;
+		PuzzleContext.Answer2 = 55291;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		var input = PuzzleContext.Input.Select(Parse2).ToArray();
 
-		return 0;
+		foreach (var val in input)
+			Console.WriteLine(val);
+
+		return input.Sum();
 	}
 
 	public long Parse(string line)
 	{
-		return long.Parse(line);
+		var digits = line.Where(c => char.IsDigit(c));
+		return long.Parse(string.Join("", digits.First(), digits.Last()));
 	}
+
+	public long Parse2(string line)
+	{
+        Console.WriteLine(line); 
+        var indexValues = new Dictionary<int, int>();
+
+        for (var i = 1; i <= 9; i++)
+        {
+	        var indexes = line.AllIndexesOf(strDigits[i-1])
+		        .Concat(line.AllIndexesOf(i.ToString()));
+	        foreach (var index in indexes)
+	        {
+		        indexValues.Add(index, i);
+	        }
+        }
+		
+		var firstDigit = indexValues[indexValues.Keys.Min()];
+		var lastDigit = indexValues[indexValues.Keys.Max()];
+		return long.Parse(string.Join("", firstDigit,lastDigit));
+	}
+
+	readonly string[] strDigits = {
+		"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+	};
 
 }
