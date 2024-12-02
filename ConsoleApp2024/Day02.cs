e9b1ec42ce2dbdd56b4597831149f5bb35bf6eb3
Thijs Kroesbergen <thijs@trilobit.nl>
2024-12-02 06:40:36 +0100

    Day-2

---
+++ ConsoleApp2024/Day02.cs
@@ -0,0 +1,89 @@
+﻿using Common;
+
+namespace ConsoleApp2024;
+
+public class Day02 : IDay
+{
+	public long Part1()
+	{
+		PuzzleContext.Answer1 = 472;
+		PuzzleContext.UseExample = false ;
+
+		var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+		long safe = 0;
+
+		foreach (var level in input)
+		{
+			var reverse = level.Reverse().ToArray();
+
+			var ok = isOk(level) || isOk(reverse);
+			if (ok)
+			{
+				safe++;
+			}
+		}
+
+		return safe;
+	}
+
+	public long Part2()
+	{
+		PuzzleContext.Answer2 = 520;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		long safe = 0;
+
+		foreach (var level in input)
+		{
+			var reverse = level.Reverse().ToArray();
+
+			var ok = isOk(level) || isOk(reverse);
+			if (ok)
+			{
+				safe++;
+			}
+			else
+			{
+				for (int i = 0; i < level.Length; i++)
+				{
+					var copy = level.ToList();
+					copy.RemoveAt(i);
+					var reversecopy = copy.ToArray().Reverse().ToArray();
+					var ok2 = isOk(copy.ToArray()) || isOk(reversecopy);
+					if (ok2)
+					{
+						safe++;
+						break;
+					}
+				}
+			}
+		}
+		
+		return safe;
+	}
+
+	private static bool isOk(long[] level)
+	{
+		var ok = true;
+		for (int i = 0; i < level.Length - 1; i++)
+		{
+			var difference = level[i + 1] - level[i];
+			if (difference is > 0 and <= 3)
+			{
+				continue;
+			}
+			ok = false;
+			break;
+		}
+
+		return ok;
+	}
+
+	private long[] Parse(string line)
+	{
+		return line.Split(' ').Select(long.Parse).ToArray();
+	}
+
+}
