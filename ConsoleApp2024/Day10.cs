38f934dc731c89430fbf80a0e1d5d43ff1a7021f
Thijs Kroesbergen <thijs@trilobit.nl>
2024-12-10 10:50:40 +0100

    Day10

---
+++ ConsoleApp2024/Day10.cs
@@ -0,0 +1,68 @@
+﻿using Common;
+
+namespace ConsoleApp2024;
+
+public class Day10 : IDay
+{
+	public long Part1()
+	{
+		PuzzleContext.Answer1 = 778;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.GetGrid<int>(c => c == '.' ? -1 : int.Parse(c.ToString()));
+
+		var trailheads = input.Find(0);
+
+		return trailheads.Sum(h => CountTrails(input, h).Count);
+	}
+
+
+	public long Part2()
+	{
+		PuzzleContext.Answer2 = 1925;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.GetGrid<int>(c => c == '.' ? -1 : int.Parse(c.ToString()));
+
+		var trailheads = input.Find(0);
+
+		return trailheads.Sum(h => CountTrails2(input, h).Count);
+	}
+
+	private HashSet<(int x, int y)> CountTrails(int[,] input, (int x, int y) start)
+	{
+		var value = input[start.x, start.y];
+
+		if (value == 9)
+		{
+			return [start];
+		}
+
+		var result = new HashSet<(int x, int y)>();
+
+		input.GetNeighbors(start.x, start.y).Where(n => input[n.x, n.y] == value + 1)
+			.SelectMany(neighbor => CountTrails(input, neighbor))
+			.ToList().ForEach(n => result.Add(n));
+
+		return result;
+	}
+
+	private List<(int x, int y)> CountTrails2(int[,] input, (int x, int y) start)
+	{
+		var value = input[start.x, start.y];
+
+		if (value == 9)
+		{
+			return [start];
+		}
+
+		var result = new List<(int x, int y)>();
+		input.GetNeighbors(start.x, start.y).Where(n => input[n.x, n.y] == value + 1)
+			.SelectMany(n => CountTrails2(input, n))
+			.ToList().ForEach(result.Add);
+
+		return result;
+	}
+
+
+}
