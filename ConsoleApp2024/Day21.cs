b7496c01c8271554199677c819f9763164af280c
Thijs Kroesbergen <thijs@trilobit.nl>
2024-12-21 11:00:39 +0100

    day 21 part 1

---
+++ ConsoleApp2024/Day21.cs
@@ -0,0 +1,161 @@
+﻿using System.Text;
+using Common;
+
+namespace ConsoleApp2024;
+
+public class Day21 : IDay
+{
+	public long Part1()
+	{
+		PuzzleContext.Answer1 = 0;
+		PuzzleContext.UseExample = false;
+
+		var codes= PuzzleContext.Input;
+
+		var numpadKeys = new[,]
+		{
+			{'7','8','9'},
+			{'4','5','6'},
+			{'1','2','3'},
+			{' ','0','A'}
+		};
+
+		var dpadKeys = new[,]
+		{
+			{' ', '^', 'A' },
+			{'<', 'v', '>' }
+		};
+
+		var numpad = new KeyPad(numpadKeys, dpadKeys);
+		var dpad1 = new KeyPad(dpadKeys, dpadKeys);
+		var dpad2 = new KeyPad(dpadKeys, dpadKeys);
+
+		var sum = 0L;
+
+
+		foreach (var code in codes)
+		{
+			ConsoleX.WriteLine(code);
+			var directions1 = numpad.GetCode(code);
+			var directions2 = dpad1.GetCode(directions1);
+			var directions3 = dpad2.GetCode(directions2);
+			ConsoleX.WriteLine(directions3);
+			var length = directions3.Length;
+
+			sum += length * int.Parse(code.Trim('A'));
+		}
+
+		return sum;
+	}
+
+	//"<v<A>A<A>>^AvAA<^A>A<v<A>>^AvA^A<vA^>A<v<A>^A>AAvA^A<v<A>A^>AAA<A>vA^A"
+	//"<vA<AA>>^  AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
+
+	//"v<<A>>^A<A>AvA<^AA>A<vAAA>^A"
+	//"<v<A>>^A<A>AvA<^AA>A<vAAA^>A"
+	//"<A^A>^^AvvvA"
+	//"<A^A>^^AvvvA"
+
+	public long Part2()
+	{
+		PuzzleContext.Answer2 = 0;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input;
+
+		return 0;
+	}
+	
+	private class KeyPad
+	{
+		private (int x, int y) _currentPosition;
+		private (int x, int y) _currentDirection;
+		private readonly char[,] _grid;
+		private readonly char[,] _dpad;
+
+		public KeyPad(char[,] grid, char[,] dpad)
+		{
+			_currentPosition = grid.Find('A').First();
+			_currentDirection = (0, 0);
+			_grid = grid;
+			_dpad = dpad;
+		}
+
+		public string GetCode(string keys)
+		{
+			var code = new StringBuilder();
+			
+			foreach (var key in keys)
+			{
+				if (key != _grid[_currentPosition.x, _currentPosition.y])
+				{
+					var (path, weight) = _grid.Dijkstra<char, ((int x, int y) position, (int x, int y) direction)>((_currentPosition, _currentDirection), GetNeighbors, s => _grid[s.position.x, s.position.y] == key);
+
+					foreach (var step in path.Select(p => p.position).Reverse())
+					{
+						code.Append(GetDirection(_currentPosition, step));
+						_currentPosition = step;
+					}
+				}
+				code.Append('A');
+			}
+			return code.ToString();
+		}
+
+		private char GetDirection((int x, int y) current, (int x, int y) step)
+		{
+			var direction = step.Subtract(current);
+
+			return DirectionToChar(direction);
+		}
+
+		private static char DirectionToChar((int, int) direction)
+		{
+			if (direction == Directions.Up)
+			{
+				return '^';
+			}
+			if (direction == Directions.Down)
+			{
+				return 'v';
+			}
+			if (direction == Directions.Left)
+			{
+				return '<';
+			}
+			if (direction == Directions.Right)
+			{
+				return '>';
+			}
+			throw new Exception("Invalid direction");
+		}
+
+		private IEnumerable<(((int x, int y) position, (int x, int y) direction) point, long weight)> GetNeighbors(char[,] grid, ((int x, int y) position, (int x, int y) direction) current)
+		{
+			return grid.GetNeighbors(current.position).Where(n => grid[n.x,n.y] != ' ').Select(n =>
+			{
+				var nd = n.Subtract(current.position);
+				return ((n, nd),
+					GetWeight(current.direction, nd));
+			});
+		}
+
+		private long GetWeight((int x, int y) current, (int x, int y) target)
+		{
+			if (current == (0, 0)) return 1;
+
+			var currentKey = DirectionToChar(current);
+			var targetKey = DirectionToChar(target);
+			if (currentKey == targetKey) return 1;
+
+			var currentPosition = _dpad.Find(currentKey).First();
+			var targetPosition = _dpad.Find(targetKey).First();
+			return currentPosition.ManhattanDistance(targetPosition)+1;
+		}
+	}
+
+}
+
+//<vA<AA>>^AvAA^<A>Av<<A>>^AvA^Av<<A>>^AAvA<A>^A<A>Av<<A>A>^AAAvA^<A>A
+//<vA<AA>>^AvAA<^A>Av<<A>>^AvA^Av<<A>>^AAvA<A>^A<A>Av<<A>A>^AAAvA<^A>A
+//<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
