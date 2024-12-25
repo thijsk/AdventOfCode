ac86103ab61496412a8d39676e6e9723bdb2e606
Thijs Kroesbergen <thijs@trilobit.nl>
2024-12-25 06:31:02 +0100

    Day 25 with Linq

--- ConsoleApp2024/Day25.cs
+++ ConsoleApp2024/Day25.cs
@@ -6,19 +6,19 @@ public class Day25 : IDay
 {
 	public long Part1()
 	{
-		PuzzleContext.Answer1 = 0;
+		PuzzleContext.Answer1 = 3307;
 		PuzzleContext.UseExample = false;
 
 		var input = PuzzleContext.Input.SplitByEmptyLines().Select(Parse).ToArray();
 
-		var keys = new List<(char[,], int[])>();
-		var locks = new List<(char[,], int[])>();
+		var keys = new List<int[]>();
+		var locks = new List<int[]>();
 
 		foreach (var grid in input)
 		{
 			var gridcolumns= grid.GetColumns();
 			var columns = new List<int>();
-			bool isLock = false;
+			var isLock = false;
 			foreach (var column in gridcolumns)
 			{
 				if (column[0] == '#')
@@ -28,33 +28,11 @@ public class Day25 : IDay
 				columns.Add(column.Count(c => c == '#')-1);
 			}
 			var list = isLock ? locks : keys;
-			list.Add((grid, columns.ToArray()));
+			list.Add(columns.ToArray());
 		}
 
-		var fit = 0L;
 
-		foreach (var @lock in locks)
-		{
-			foreach (var key in keys)
-			{
-				ConsoleX.WriteLine($"{string.Join(",",@lock.Item2)} {string.Join(",", key.Item2)}");
-
-				var fits = true;
-				for (int c = 0; c < key.Item2.Length; c++)
-				{
-					if (key.Item2[c] + @lock.Item2[c] > 5)
-					{
-						fits = false;
-					}
-				}
-				if (fits)
-				{
-					fit++;
-				}
-			}
-		}
-
-		return fit;
+		return locks.SelectMany(l => keys, (l, k) => (l, k)).Count(pair => Enumerable.Select(pair.k, (p, i) => (p, i)).All((p) => pair.l[p.i] + p.p <= 5));
 	}
 
 	public long Part2()
