2be1863acdc60a97df56f51279dc141ad1a2a4aa
Thijs Kroesbergen <thijs@trilobit.nl>
2025-12-01 08:34:44 +0100

    Add ConsoleApp2025 project and update solution
    Updated AdventOfCode.sln to support Visual Studio Version 18 and added a new project, ConsoleApp2025. Created ConsoleApp2025.csproj targeting .NET 10.0, with references to a common project and text files for output. Added Day00.cs, Day01.cs, and Day02.cs under ConsoleApp2025 namespace, implementing the IDay interface. Updated Program.cs to run the latest IDay class and copy results to the clipboard.

---
+++ ConsoleApp2025/Day01.cs
@@ -0,0 +1,79 @@
+﻿using Common;
+
+namespace ConsoleApp2025;
+
+public class Day01 : IDay
+{
+	public long Part1()
+	{
+		PuzzleContext.Answer1 = 1105;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+		var dial = 50;
+		var password = 0L;
+
+		foreach ((var direction, int amount) in input)
+		{
+			if (direction == 'R')
+			{
+				dial += amount;
+			}
+			else
+			{
+				dial -= amount;
+
+			}
+			dial %= 100;
+
+			if (dial == 0)
+				password++;
+		}
+
+		return password;
+	}
+
+	public long Part2()
+	{
+		PuzzleContext.Answer2 = 6599;
+		PuzzleContext.UseExample = false;
+
+		var input = PuzzleContext.Input.Select(Parse).ToArray();
+
+		var dial = 50;
+		var password = 0L;
+
+		foreach ((var direction, int amount) in input)
+		{
+			var step = (direction == 'R') ? 1 : -1;
+			for (int i = 0; i < amount; i++)
+			{
+				dial += step;
+				if (step == 1 && dial == 100)
+				{
+					dial = 0;
+				}
+				else if (step == -1 && dial == -1)
+				{
+					dial = 99;
+				}
+				if (dial == 0)
+				{
+					password++;
+				}
+			}
+		}
+
+		return password;
+	}
+
+	private (char direction, int amount) Parse(string line)
+	{
+		var direction = line[0];
+		var amount = int.Parse(line[1..]);
+
+		return (direction, amount);
+	}
+
+}
---
+++ ConsoleApp2025/Program.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using TextCopy;
+
+var dayClasses = DayRunner.GetAllIDays();
+var dayClass = dayClasses
+	//.Where(d => d.Name == "Day07")
+	.Last();
+
+var runner = new DayRunner(dayClass);
+var answer = runner.RunPart1();
+if (answer != 0) ClipboardService.SetText(answer.ToString());
+answer = runner.RunPart2();
+if (answer != 0) ClipboardService.SetText(answer.ToString());
