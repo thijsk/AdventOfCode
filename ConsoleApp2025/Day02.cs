d7636fca0562dab12e16fbc2799238e5ebdffb37
Thijs Kroesbergen <thijs@trilobit.nl>
2025-12-02 12:03:21 +0100

    Day02

--- ConsoleApp2025/Day02.cs
+++ ConsoleApp2025/Day02.cs
@@ -6,27 +6,104 @@ public class Day02 : IDay
 {
 	public long Part1()
 	{
-		PuzzleContext.Answer1 = 0;
+		PuzzleContext.Answer1 = 43952536386;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
-		
-		return 0L;
+		var input = PuzzleContext.Input.SelectMany(Parse);
+
+
+		Console.WriteLine("start");
+
+		var result = 0L;
+		HashSet<long> found = new();
+		foreach (var (r1, r2) in input)
+		{
+			// find ranges where any of numbers have repeating digits
+			for (var n = r1; n <= r2; n++)
+			{
+				if (IsRepeatingPattern(n, 2))
+				{
+					Console.WriteLine($"Found repeating pattern: {n}");
+					if (!found.Add(n))
+					{
+						Console.WriteLine($"Already found: {n}");
+					}
+					result += n;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	private bool IsRepeatingPattern(long r1, int splits)
+	{
+		var rstring = r1.ToString();
+		if (rstring.Length % splits != 0)
+		{
+			return false;
+		}
+		var period = rstring.Length / splits;
+
+
+		var pattern = rstring.Substring(0, period);
+		for (var i = 1; i < splits; i++)
+		{
+			var part = rstring.Substring(i * period, period);
+			if (part != pattern)
+			{
+				return false;
+			}
+		}
+
+		return true;
+
 	}
 
 	public long Part2()
 	{
-		PuzzleContext.Answer2 = 6599;
+		PuzzleContext.Answer2 = 0;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		var input = PuzzleContext.Input.SelectMany(Parse);
 
-		return 0L;
+		Console.WriteLine("start");
+
+		var result = 0L;
+		HashSet<long> found = new();
+		foreach (var (r1, r2) in input)
+		{
+			for (var n = r1; n <= r2; n++)
+			{
+				for (int l = 2; l <= 10; l++)
+				{
+					if (IsRepeatingPattern(n, l))
+					{
+						Console.WriteLine($"Found repeating pattern: {n}");
+						if (!found.Add(n))
+						{
+							Console.WriteLine($"Already found: {n}");
+						}
+						else
+							result += n;
+					}
+				}
+			}
+		}
+
+		Console.WriteLine(found.Sum());
+		return result;
 	}
 
-	private object Parse(string line)
+	private IEnumerable<(long, long)> Parse(string line)
 	{
-		return null;
+		var parts = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+		foreach (var part in parts)
+		{
+			Console.WriteLine($"Parsing part: {part}");
+			var rangeParts = part.Split('-');
+			yield return (long.Parse(rangeParts[0]), long.Parse(rangeParts[1]));
+		}
 	}
 
 }
