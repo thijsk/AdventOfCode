6829816d4f01dde2bb8b5ab5bd0253af216d6dd0
Thijs Kroesbergen <thijs@trilobit.nl>
2025-12-04 08:31:36 +0100

    Day04

--- ConsoleApp2025/Day04.cs
+++ ConsoleApp2025/Day04.cs
@@ -6,27 +6,66 @@ public class Day04 : IDay
 {
 	public long Part1()
 	{
-		PuzzleContext.Answer1 = 0;
+		PuzzleContext.Answer1 = 1460;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
-		
-		return 0L;
+		var grid = Parse(PuzzleContext.Input);
+
+		var result = 0L;
+
+		char[,]? output = RemoveRolls(grid, ref result);
+
+
+		return result;
+	}
+
+	private static char[,] RemoveRolls(char[,] grid, ref long result)
+	{
+		var output = (char[,])grid.Clone();
+		foreach (var (x, y) in grid.GetIndexes())
+		{
+			if (grid[x, y] != '@')
+				continue;
+			var neighbors = grid.GetAllNeighbors(x, y);
+			var rolls = neighbors.Count(n => grid[n.x, n.y] == '@');
+			if (rolls < 4)
+			{
+				output[x, y] = 'X';
+				result++;
+			}
+		}
+
+		output.ToConsole();
+
+		return output;
 	}
 
 	public long Part2()
 	{
-		PuzzleContext.Answer2 = 0;
+		PuzzleContext.Answer2 = 9243;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		var grid = Parse(PuzzleContext.Input);
 
-		return 0L;
+		var result = 0L;
+
+		var before = 0L;
+
+		do
+		{
+			before = result;
+			grid = RemoveRolls(grid, ref result);
+
+			grid.ToConsole();
+
+		} while (before != result);
+
+		return result;
 	}
 
-	private object Parse(string line)
+	private char[,] Parse(string[] lines)
 	{
-		return null;
+		return lines.GetGrid();
 	}
 
 }
