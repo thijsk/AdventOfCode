16b4c1bae3e107b6f1e7bc8f52405dd8f9bd560c
Thijs Kroesbergen <thijs@trilobit.nl>
2025-12-05 08:34:41 +0100

    day05

--- ConsoleApp2025/Day05.cs
+++ ConsoleApp2025/Day05.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using System;
 
 namespace ConsoleApp2025;
 
@@ -9,24 +10,56 @@ public class Day05 : IDay
 		PuzzleContext.Answer1 = 0;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
-		
-		return 0L;
-	}
+		var (ranges, ingredients) = Parse(PuzzleContext.Input);
+
+		return ingredients.Count(i => ranges.Any(r => r.start <= i && r.end >= i));
+	}		
 
 	public long Part2()
 	{
 		PuzzleContext.Answer2 = 0;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		var (ranges, ingredients) = Parse(PuzzleContext.Input);
 
-		return 0L;
+		var last = 0L;
+		var result = new List<(long start, long end)>();
+		foreach (var range in ranges.OrderBy(r => r.start))
+		{
+			if (range.start <= last)
+			{
+				if (range.end > last)
+				{
+					var newlast = result[^1];
+					newlast.end = range.end;
+					result[^1] = newlast;
+					last = newlast.end;
+				}
+			}
+			else
+			{
+				result.Add(range);
+				last = range.end;
+			}
+		}
+
+		return result.Sum(r => (r.end - r.start) + 1);
 	}
 
-	private object Parse(string line)
+	private ((long start, long end)[] ranges, long[] ingredients) Parse(string[] lines)
 	{
-		return null;
+		var (ranges, ingredients) = lines.SplitByEmptyLines();
+
+		var parsedRanges = ranges.Select(r =>
+		{
+			var split = r.Split('-').Select(long.Parse).ToArray();
+			return (split[0], split[1]);
+		}).ToArray();
+
+		var parsedIngredients = ingredients.Select(long.Parse).ToArray();
+
+		return (parsedRanges, parsedIngredients);
+
 	}
 
 }
