7858c333847aebaecde877afc4a863f7797804cf
Thijs Kroesbergen <thijs@trilobit.nl>
2025-12-07 09:12:45 +0100

    day07

--- ConsoleApp2025/Day07.cs
+++ ConsoleApp2025/Day07.cs
@@ -2,31 +2,95 @@
 
 namespace ConsoleApp2025;
 
-public class Day00 : IDay
+public class Day07 : IDay
 {
 	public long Part1()
 	{
-		PuzzleContext.Answer1 = 0;
+		PuzzleContext.Answer1 = 1507;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
-		
-		return 0L;
+		var input = PuzzleContext.Input.GetGrid();
+
+		var count = 0L;
+
+		for (int r = 0; r < input.GetRowCount(); r++)
+		{
+			for (int c = 0; c < input.GetColumnCount(); c++)
+			{
+				var cell = input[r, c];
+				
+				if (r == 0)
+				{
+					if (cell == 'S')
+					{
+						input[r, c] = '|';
+					}
+					
+				} else
+				{
+					var above = input[r - 1, c];
+					if (above == '|')
+					{
+						if (cell == '^')
+						{
+							input[r, c - 1] = '|';
+							input[r, c + 1] = '|';
+							count++;
+
+						} else if (cell == '.')
+						{
+							input[r, c] = '|';
+						}
+					}
+				}
+			}
+		}
+
+
+		input.ToConsole();
+
+		return count;
 	}
 
 	public long Part2()
 	{
-		PuzzleContext.Answer2 = 0;
+		PuzzleContext.Answer2 = 1537373473728;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		var input = PuzzleContext.Input.GetGrid();
 
-		return 0L;
+
+		var s = (0, input.GetRow(0).IndexOf('S'));
+		var count = GetCount(input, s, input.GetRowCount()-1);
+
+		return count;
 	}
 
-	private object Parse(string line)
+	private Dictionary<(int, int), long> cache = new();
+
+	private long GetCount(char[,] input, (int r, int c) s, int maxr)
 	{
-		return null;
-	}
+		if (s.r == maxr)
+			return 1;
 
+		long result;
+		if (cache.TryGetValue(s, out result))
+		{
+			return result;
+		}
+
+		if (input[s.r, s.c] == '^')
+		{
+			var left = GetCount(input, (s.r+1, s.c- 1), maxr);
+			var right = GetCount(input, (s.r+1, s.c+ 1), maxr);
+			cache.Add(s, left + right);
+			return left + right;
+		}
+		else
+		{
+			var below = GetCount(input, (s.r + 1, s.c), maxr);
+			cache.Add(s, below);
+			return below;
+		}
+	}
 }
