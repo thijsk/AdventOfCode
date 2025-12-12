30501d45a4b1a2bfd7335e5c18d18a3047c7db92
Thijs Kroesbergen <thijs@trilobit.nl>
2025-12-12 11:31:47 +0100

    day 12

--- ConsoleApp2025/Day12.cs
+++ ConsoleApp2025/Day12.cs
@@ -1,4 +1,6 @@
 ﻿using Common;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ConsoleApp2025;
 
@@ -6,12 +8,39 @@ public class Day12 : IDay
 {
 	public long Part1()
 	{
-		PuzzleContext.Answer1 = 0;
+		PuzzleContext.Answer1 = 499;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
-		
-		return 0L;
+		var (pieces, regions) = Parse(PuzzleContext.Input);
+
+		var willFit = 0L;
+		var wontFit = 0L;
+		var mayFit = 0L;
+
+		foreach (var region in regions)
+		{
+			var brutoSizeNeeded = region.Presents.Select((c, i) => c * pieces[i].BrutoArea).Sum();
+			var nettoSizeNeeded = region.Presents.Select((c, i) => c * pieces[i].NettoArea).Sum();
+
+			if (brutoSizeNeeded <= region.Area)
+			{
+				willFit++;
+			}
+			else if (nettoSizeNeeded > region.Area)
+			{
+				wontFit++;
+			}
+			else
+			{
+				mayFit++;
+			}
+		}
+
+		// That is lucky, there are no regions in the "may fit" category
+		Debug.Assert(mayFit == 0);
+		Debug.Assert(willFit + wontFit + mayFit == regions.Count);
+
+		return willFit;
 	}
 
 	public long Part2()
@@ -19,14 +48,82 @@ public class Day12 : IDay
 		PuzzleContext.Answer2 = 0;
 		PuzzleContext.UseExample = false;
 
-		var input = PuzzleContext.Input.Select(Parse).ToArray();
+		//var input = PuzzleContext.Input.Select(Parse).ToArray();
 
 		return 0L;
 	}
 
-	private object Parse(string line)
+	private (List<Present> presents, List<Region> regions) Parse(string[] lines)
 	{
-		return null;
+		var parts = lines.SplitByEmptyLines();
+
+		var rPresents = new List<Present>();
+		var rRegions = new List<Region>();
+
+		foreach (var part in parts)
+		{
+			if (part[0].EndsWith(":"))
+			{
+				// Present
+				var present = new Present();
+				present.Name = part[0].TrimEnd(':');
+				present.Shape = part[1..].GetGrid();
+
+				rPresents.Add(present);
+			}
+			else
+			{
+				// Regions part
+
+				foreach (var rpart in part)
+				{
+					var region = new Region();
+					var rparts = rpart.Split(":");
+					var wxh = rparts[0].Split("x");
+					region.Width = int.Parse(wxh[0]);
+					region.Height = int.Parse(wxh[1]);
+					region.Presents = rparts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToArray();
+					rRegions.Add(region);
+				}
+			}
+		}
+		return (rPresents, rRegions);
+
 	}
 
+
+
+}
+
+internal class Region
+{
+	public int Width { get; internal set; }
+	public int Height { get; internal set; }
+	public int[] Presents { get; internal set; }
+
+	public int Area => Width * Height;
+}
+
+internal class Present
+{
+	public string Name { get; internal set; }
+	public char[,] Shape { get; internal set; }
+
+	public int BrutoArea
+	{
+		get
+		{
+			var area = Shape.GetLength(0) * Shape.GetLength(1);
+			return area;
+		}
+	}
+
+	public int NettoArea
+	{
+		get
+		{
+			var area = Shape.GetIndexes().Count(i => Shape[i.x, i.y] == '#');
+			return area;
+		}
+	}
 }
